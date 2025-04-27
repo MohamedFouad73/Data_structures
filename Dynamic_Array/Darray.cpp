3a37f8c343e3e13f4c64084ac5a4bb565ebee4d5
unknown <mohamedfouadbus@gmail.com>
2025-04-27 13:46:14 +0300

    adding files to data structures repo

---
+++ Dynamic_Array/Darray.cpp
@@ -0,0 +1,6 @@
+//
+// Created by DELL on 27/04/2025.
+//
+
+#include "Darray.h"
+
---
+++ Dynamic_Array/Darray.h
@@ -0,0 +1,27 @@
+//
+// Created by DELL on 27/04/2025.
+//
+
+#ifndef DARRAY_H
+#define DARRAY_H
+
+
+template <class t>
+class Darray {
+    int size; // reserved places
+    int length; // assigned numbers
+    int* access_ptr; // to access the values stored in heap
+    public :
+    Darray(int arr_size): size(arr_size)
+
+    
+};
+
+
+
+#endif //DARRAY_H
+
+// template <class t>
+// inline Darray<t>::Darray(int arr_size)
+// {
+// }
